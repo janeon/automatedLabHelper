8cb56dd86797919f0dc67ffe67ea4cac168a76ef
Emily Hamlin <Emily@ip-69-123.wireless.oberlin.edu>
2019-05-06 11:47:12 -0400

    adding to regexChecker.py

--- regexChecks.py
+++ regexChecks.py
@@ -6,27 +6,36 @@
         3 forgetting "()" at the end of a function call
         4 forgetting a ":" at the end of a for loop
         5 forgetting a ":" at the end of a conditional statement
+        6 forgetting a ":" at the end of a while loop
+        7 using () instead of [] to call an index of a list
+            this one is commented out b/c i'm not totally sure about it. it would work in a specific case, but not all
 
-        others: while loop colon,
+        others we could implement: stuff for incorrect bracket types???
 """
 import re
-regex1 = re.compile("(False|None|True|and|as|assert|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield) = [a-zA-Z0-9-+_\[\]{}(),:]*")
-regex2 = re.compile("def [a-zA-Z_][a-zA-Z0-9_]*\(\)$")
-regex3 = re.compile("def [a-zA-Z_][a-zA-Z0-9_]*:")
-#regex4 = regex1
-#regex5 = regex1
+regex1 = re.compile("^(False|None|True|and|as|assert|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield) = [a-zA-Z0-9-+_\[\]{}(),:]*")
+regex2 = re.compile("^def [a-zA-Z_][a-zA-Z0-9_]*\(\)$")
+regex3 = re.compile("^def [a-zA-Z_][a-zA-Z0-9_]*:")
+regex4 = re.compile("^for .* in [^:\n]*$")
+regex5 = re.compile("^(if |elif |else)[^:\n]*$")
+regex6 = re.compile("^while [^:\n]*$")
+#regex7 = re.compile("^[a-zA-Z_][a-zA-Z0-9_]* = [a-zA-Z_][a-zA-Z0-9_]*\(.*\)$")
 
 def check(line):
     if (regex1.match(line)):
-        return "This error occured because you tried to use a python keyword in your variable assignment."
+        return "Looks like you tried to use a python keyword in your variable assignment."
     elif (regex2.match(line)):
-        return "This error occured because you forgot a colon at the end of your function call."
+        return "Looks like you forgot a colon at the end of your function call."
     elif (regex3.match(line)):
-        return "This error occured because you forgot the parentheses after your function name."
-    #elif (regex4.match(line)):
-        #return "This error occured because you forgot a colon at the end of your for loop."
-    #elif (regex5.match(line)):
-        #return "This error occured because you forgot a colon at the end of your conditional statement."
+        return "Looks like you forgot the parentheses after your function name."
+    elif (regex4.match(line)):
+        return "Looks like you forgot a colon at the end of your for loop."
+    elif (regex5.match(line)):
+        return "Looks like you forgot a colon at the end of your conditional statement."
+    elif (regex6.match(line)):
+        return "Looks like you forgot a colon at the end of your while loop."
+    #elif (regex7.match(line)):
+        #return "Looks like you tried to use () rather than [] when accessing an array item"
     return ""
 
 #def main():
@@ -38,22 +47,3 @@ def check(line):
     #print("check for ","\"",l3,"\""," is ",check(l3))
 
 #main()
-
-
-
-
-#test_str = ("for i in range(2):\n"
-	#"for = 5\n"
-	#"fdsa = 2\n"
-	#"continue = 3\n\n\n\n")
-
-#matches = re.finditer(regex, test_str, re.MULTILINE)
-
-#for matchNum, match in enumerate(matches, start=1):
-
-    #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-
-    #for groupNum in range(0, len(match.groups())):
-        #groupNum = groupNum + 1
-
-        #print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
